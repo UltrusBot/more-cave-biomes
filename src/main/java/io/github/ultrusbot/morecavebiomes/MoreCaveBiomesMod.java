3107a8d46c1252eacdf3adf44833b9c920d8c5de
ultra <13109003+UltrusBot@users.noreply.github.com>
2021-07-03 11:28:23 -0400

    Added Base Mod

---
+++ src/main/java/io/github/ultrusbot/morecavebiomes/MoreCaveBiomesMod.java
@@ -0,0 +1,11 @@
+package io.github.ultrusbot.morecavebiomes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MoreCaveBiomesMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
